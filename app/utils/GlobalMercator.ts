5de92db660ba914ba6791cea7be0686e034f15fb
Denis <carriere.denis@gmail.com>
2016-07-20 21:11:42 -0400

    Add Typescript for GlobalMercator

--- app/utils/GlobalMercator.ts
+++ app/utils/GlobalMercator.ts
@@ -1,9 +1,14 @@
 import { range } from 'lodash'
 
-export class tile {
-  public x: number
-  public y: number
-  public zoom: number
+export const bounds = (init:number[]) => {
+  if (init.length !== 4) { throw new Error('[bounds] Must be an array with 4x Numbers.')}
+  return [...init]
+}
+
+export class google {
+  x: number
+  y: number
+  zoom: number
   constructor(init: {x:number, y:number, zoom:number}) {
     const {x, y, zoom} = init
     this.x = x
@@ -12,41 +17,53 @@ export class tile {
   }
 }
 
+export class tile {
+  tx: number
+  ty: number
+  zoom: number
+  constructor(init: {tx:number, ty:number, zoom:number}) {
+    const {tx, ty, zoom} = init
+    this.tx = tx
+    this.ty = ty
+    this.zoom = zoom
+  }
+}
+
 export class pixels {
-  public px: number
-  public py: number
-  public zoom: number
+  px: number
+  py: number
+  zoom: number
   constructor(init: {px:number, py:number, zoom?:number}) {
     const {px, py, zoom} = init
     this.px = px
     this.py = py
-    this.zoom = zoom
+    if (zoom) { this.zoom = zoom }
   }
 }
 
 export class meters {
-  public mx: number
-  public my: number
-  public zoom: number
+  mx: number
+  my: number
+  zoom: number
   constructor(init: {mx:number, my:number, zoom?:number}) {
     const {mx, my, zoom} = init
     this.mx = mx
     this.my = my
-    this.zoom = zoom
+    if (zoom) { this.zoom = zoom }
   }
 }
 
 export class latlng {
-  public lat:number
-  public lng:number
-  public zoom:number
+  lat:number
+  lng:number
+  zoom:number
   constructor(init: {lat:number, lng:number, zoom?:number}) {
     const {lat, lng, zoom} = init
     this.lat = lat
     this.lng = lng
     this.zoom = zoom
-    if (lat > -90 && lat < 90) { throw new Error('[lat] must be within -90 to 90 degrees')}
-    if (lng > -180 && lng < 180) { throw new Error('[lng] must be within -180 to 180 degrees')}
+    if (lat < -90 || lat > 90) { throw new Error('[lat] must be within -90 to 90 degrees')}
+    if (lng < -180 || lng > 180) { throw new Error('[lng] must be within -180 to 180 degrees')}
   }
 }
 
@@ -94,7 +111,6 @@ export default class GlobalMercator {
    */
   LatLonToMeters(init:latlng) {
     const { lat, lng, zoom } = new latlng(init)
-
     let mx: number = lng * this.originShift / 180.0
     let my: number = Math.log(Math.tan((90 + lat) * Math.PI / 360.0 )) / (Math.PI / 180.0)
     my = my * this.originShift / 180.0
@@ -114,7 +130,6 @@ export default class GlobalMercator {
     const {mx, my, zoom} = new meters(init)
     let lng = (mx / this.originShift) * 180.0
     let lat = (my / this.originShift) * 180.0
-
     lat = 180 / Math.PI * (2 * Math.atan( Math.exp( lat * Math.PI / 180.0)) - Math.PI / 2.0)
 
     return new latlng({ lat: lat, lng: lng, zoom:zoom })
@@ -146,7 +161,7 @@ export default class GlobalMercator {
    * @returns {tile}
    */
   MetersToTile(init:meters) {
-    const pixels = this.MetersToPixels(init)
+    const pixels = this.MetersToPixels(new meters(init))
 
     return this.PixelsToTile(pixels)
   }
@@ -176,121 +191,143 @@ export default class GlobalMercator {
    * @param {Number} px
    * @param {Number} py
    * @param {Number} zoom
-   * @returns {px:number, py:number}
+   * @returns {tile}
    */
   PixelsToTile(init:pixels) {
     const {px, py, zoom} = new pixels(init)
+    const tx = Math.ceil(px / this.tileSize) - 1
+    const ty = Math.ceil(py / this.tileSize) - 1
 
-    if (typeof px == 'undefined') { throw new Error('[px] required') }
-    if (typeof py == 'undefined') { throw new Error('[py] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
-    let tx = Math.ceil(px / parseFloat(this.tileSize)) - 1
-    let ty = Math.ceil(py / parseFloat(this.tileSize)) - 1
-
-    return { tx: tx, ty: ty, zoom: zoom }
+    return new tile({ tx: tx, ty: ty, zoom: zoom })
   }
 
-
-  TileBounds({ tx, ty, zoom }) {
-    // Returns bounds of the given tile in EPSG:900913 coordinates
-
-    if (typeof tx == 'undefined') { throw new Error('[tx] required') }
-    if (typeof ty == 'undefined') { throw new Error('[ty] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
+  /**
+   * Returns bounds of the given tile in EPSG:900913 coordinates
+   * 
+   * @name TileBounds
+   * @param {Number} tx
+   * @param {Number} ty
+   * @param {Number} zoom
+   * @returns {bounds}
+   */
+  TileBounds(init:tile) {
+    const {tx, ty, zoom} = new tile(init)
     let min = this.PixelsToMeters({ px: tx * this.tileSize, py: ty * this.tileSize, zoom: zoom })
     let max = this.PixelsToMeters({ px: (tx + 1) * this.tileSize, py: (ty + 1) * this.tileSize, zoom: zoom })
 
-    return [ min.mx, min.my, max.mx, max.my ]
+    return bounds([ min.mx, min.my, max.mx, max.my ])
   }
 
-
-  TileLatLonBounds({ tx, ty, zoom }) {
-    // Returns bounds of the given tile in latutude/longitude using WGS84 datum
-
-    if (typeof tx == 'undefined') { throw new Error('[tx] required') }
-    if (typeof ty == 'undefined') { throw new Error('[ty] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
-    let bounds = this.TileBounds({ tx: tx, ty: ty, zoom:zoom })
-    let min = this.MetersToLatLon({ mx: bounds[0], my: bounds[1] })
-    let max = this.MetersToLatLon({ mx: bounds[2], my: bounds[3] })
-
-    return [ min.lng, min.lat, max.lng, max.lat ]
+  /**
+   * Returns bounds of the given tile in EPSG:900913 coordinates
+   * 
+   * @name TileLatLonBounds
+   * @param {Number} tx
+   * @param {Number} ty
+   * @param {Number} zoom
+   * @returns {bounds}
+   */
+  TileLatLonBounds(init:tile) {
+    const {tx, ty, zoom} = new tile(init)
+    const [mx1, my1, mx2, my2] = this.TileBounds({ tx: tx, ty: ty, zoom:zoom })
+    const min = this.MetersToLatLon({ mx: mx1, my: my1, zoom:zoom })
+    const max = this.MetersToLatLon({ mx: mx2, my: my2, zoom:zoom })
+    
+    return bounds([ min.lng, min.lat, max.lng, max.lat ])
   }
 
-  GoogleBounds({ x, y, zoom }) {
-    // Converts Google tile system in Mercator bounds (meters)
-
-    if (typeof x == 'undefined') { throw new Error('[x] required') }
-    if (typeof y == 'undefined') { throw new Error('[y] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
-    let tile = this.GoogleTile({ x: x, y: y, zoom: zoom })
+  /**
+   * Converts Google tile system in Mercator bounds (meters)
+   * 
+   * @name GoogleBounds
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} zoom
+   * @returns {bounds}
+   */
+  GoogleBounds(init:google) {
+    const tile = this.GoogleTile(init)
     return this.TileBounds(tile)
   }
 
-  GoogleLatLonBounds({ x, y, zoom }) {
-    // Converts Google tile system in LatLng bounds (degrees)
-
-    if (typeof x == 'undefined') { throw new Error('[x] required') }
-    if (typeof y == 'undefined') { throw new Error('[y] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
-    let tile = this.GoogleTile({ x: x, y: y, zoom: zoom })
+  /**
+   * Converts Google tile system in LatLng bounds (degrees)
+   * 
+   * @name GoogleLatLonBounds
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} zoom
+   * @returns {bounds}
+   */
+  GoogleLatLonBounds(init:google) {
+    const tile = this.GoogleTile(init)
     return this.TileLatLonBounds(tile)
   }
 
-  TileGoogle({ tx, ty, zoom }) {
-    // Converts TMS tile coordinates to Google Tile coordinates
+  /**
+   * Converts TMS tile coordinates to Google Tile coordinates
+   * 
+   * @name TileGoogle
+   * @param {Number} tx
+   * @param {Number} ty
+   * @param {Number} zoom
+   * @returns {bounds}
+   */
+  TileGoogle(init:tile) {
+    const { tx, ty, zoom } = new tile(init)
+    const x = tx
+    const y = (Math.pow(2, zoom) - 1) - ty
 
-    if (typeof tx == 'undefined') { throw new Error('[tx] required') }
-    if (typeof ty == 'undefined') { throw new Error('[ty] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
-    let x = tx
-    let y = (Math.pow(2, zoom) - 1) - ty
-
-    return { x: x, y: y, zoom: zoom }
+    return new google({ x: x, y: y, zoom: zoom })
   }
 
-  GoogleTile({ x, y, zoom }) {
-    // Converts Google Tile coordinates to TMS tile coordinates
-
-    if (typeof x == 'undefined') { throw new Error('[x] required') }
-    if (typeof y == 'undefined') { throw new Error('[y] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
-    let tx = x
-    let ty = Math.pow(2, zoom) - y - 1
-
-    return { tx: tx, ty: ty, zoom: zoom }
+  /**
+   * Converts Google Tile coordinates to TMS tile coordinates
+   * 
+   * @name GoogleTile
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} zoom
+   * @returns {tile}
+   */
+  GoogleTile(init:google) {
+    const { x, y, zoom } = new google(init)
+    const tx = x
+    const ty = Math.pow(2, zoom) - y - 1
+  
+    return new tile({ tx: tx, ty: ty, zoom: zoom })
   }
 
-  GoogleQuadKey({ x, y, zoom }) {
-    // Converts Google Tile coordinates to Microsoft QuadKey
-
-    if (typeof x == 'undefined') { throw new Error('[x] required') }
-    if (typeof y == 'undefined') { throw new Error('[y] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
-    let tile = this.GoogleTile({ x: x, y: y, zoom: zoom })
+  /**
+   * Converts Google Tile coordinates to Microsoft QuadKey
+   * 
+   * @name GoogleTile
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} zoom
+   * @returns {quadkey}
+   */
+  GoogleQuadKey(init:google) {
+    const tile = this.GoogleTile(init)
     return this.TileQuadKey(tile)
   }
 
-  TileQuadKey({ tx, ty, zoom }) {
-    // Converts TMS tile coordinates to Microsoft QuadKey
-
-    if (typeof tx == 'undefined') { throw new Error('[tx] required') }
-    if (typeof ty == 'undefined') { throw new Error('[ty] required') }
-    if (typeof zoom == 'undefined') { throw new Error('[zoom] required') }
-
+  /**
+   * Converts TMS tile coordinates to Microsoft QuadKey
+   * 
+   * @name TileQuadKey
+   * @param {Number} tx
+   * @param {Number} ty
+   * @param {Number} zoom
+   * @returns {quadkey}
+   */
+  TileQuadKey(init:tile) {
+    let { tx, ty, zoom } = new tile(init)
     let quadkey = ''
-    ty = (Math.pow(2, zoom) - 1) - ty
 
+    ty = (Math.pow(2, zoom) - 1) - ty
     range(zoom, 0, -1).map(i => {
-      let digit = 0
+      let digit:string = '0'
       let mask = 1 << (i - 1)
       if ((tx & mask) != 0) { digit += 1 }
       if ((ty & mask) != 0) { digit += 2 }
@@ -300,24 +337,29 @@ export default class GlobalMercator {
     return quadkey
   }
 
-  QuadKeyTile(quadkey) {
-    // Converts QuadKey to TMS tile coordinates
-
-    if (typeof quadkey !== 'string') { throw new Error('[quadkey] must be string') }
-
-    let google = this.QuadKeyGoogle(quadkey)
-
+  /**
+   * Converts QuadKey to TMS tile coordinates
+   * 
+   * @name QuadKeyTile
+   * @param {String} quadkey
+   * @returns {tile}
+   */
+  QuadKeyTile(quadkey:string) {
+    const google = this.QuadKeyGoogle(quadkey)
     return this.GoogleTile(google)
   }
 
-  QuadKeyGoogle(quadkey) {
-    // Converts QuadKey to Google tile
-
-    if (typeof quadkey !== 'string') { throw new Error('[quadkey] must be string') }
-
+  /**
+   * Converts QuadKey to Google tile
+   * 
+   * @name QuadKeyGoogle
+   * @param {String} quadkey
+   * @returns {google}
+   */
+  QuadKeyGoogle(quadkey:string) {
     let x = 0
     let y = 0
-    let zoom = quadkey.length
+    const zoom = quadkey.length
 
     range(zoom, 0, -1).map(i => {
       let mask = 1 << (i - 1)
@@ -339,7 +381,7 @@ export default class GlobalMercator {
         throw new Error('Invalid QuadKey digit sequence')
       }
     })
-    return { x: x, y: y, zoom: zoom }
+    return new google({ x: x, y: y, zoom: zoom })
   }
 }
 export const mercator = new GlobalMercator()
