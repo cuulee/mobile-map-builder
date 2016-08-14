0d2559dfbc0123562e5a89b4cd32a21c539f67b1
Denis <carriere.denis@gmail.com>
2016-08-13 20:38:57 -0400

    Add Debug typings

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -3,16 +3,19 @@ import _GlobalMercator from './GlobalMercator'
 import _Grid from './Grid'
 import _MBTiles from './MBTiles'
 import _Tile from './Tile'
+import _debug from './debug'
 
 export const GeoJSON = _GeoJSON
 export const GlobalMercator = _GlobalMercator
 export const Grid = _Grid
 export const MBTiles = _MBTiles
 export const Tile = _Tile
+export const debug = _debug
 export default {
   GeoJSON: GeoJSON,
   GlobalMercator: GlobalMercator,
   Grid: Grid,
   MBTiles: MBTiles,
   Tile: Tile,
+  debug: _debug
 }
