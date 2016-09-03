686f70952da42f31ac71efd04993e579665d6aa5
Denis <carriere.denis@gmail.com>
2016-09-03 11:48:35 -0400

    Remove GeoJSON class

--- app/index.ts
+++ app/index.ts
@@ -1,18 +1,15 @@
-import _GeoJSON from './GeoJSON'
 import _GlobalMercator from './GlobalMercator'
 import _Grid from './Grid'
 import _MBTiles from './MBTiles'
 import _Tile from './Tile'
 import _debug from './debug'
 
-export const GeoJSON = _GeoJSON
 export const GlobalMercator = _GlobalMercator
 export const Grid = _Grid
 export const MBTiles = _MBTiles
 export const Tile = _Tile
 export const debug = _debug
 export default {
-  GeoJSON,
   GlobalMercator,
   Grid,
   MBTiles,
