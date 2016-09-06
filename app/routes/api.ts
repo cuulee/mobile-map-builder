3fddb5fa7b33f1e229908447bba3906045545a90
Denis <carriere.denis@gmail.com>
2016-09-05 22:24:02 -0400

    Modular datasets

--- app/routes/api.ts
+++ app/routes/api.ts
@@ -1,4 +1,7 @@
 import * as turf from 'turf'
+import * as yaml from 'js-yaml'
+import * as fs from 'fs'
+import { keys } from 'lodash'
 import { Router, Request, Response } from 'express'
 import { worker } from 'cluster'
 import * as rp from 'request-promise'
@@ -7,6 +10,16 @@ import debug from '../debug'
 import { geojson2osm } from 'geojson2osm'
 
 const router = Router()
+
+interface InterfaceRequest extends Request {
+  params: {
+    ext: string
+    tile_row: number
+    tile_column: number
+    zoom: number
+    dataset: string
+  }
+}
 
 async function downloadData(url: string): Promise<GeoJSON.FeatureCollection<any>> {
   debug.download(url)
@@ -17,79 +30,81 @@ async function downloadData(url: string): Promise<GeoJSON.FeatureCollection<any>
     )
 }
 
-const ballDiamonds = downloadData(
-  'https://gist.githubusercontent.com/DenisCarriere/4d0ebc8126eb0c4bffe7b0bd5ee029c9/raw/' +
-  '2af406db35332898aae1aa72c0aae68188504b59/ball-diamonds.geojson')
+// Build datasets
+const datasets: any = {}
+const data = yaml.safeLoad(fs.readFileSync(`${ __dirname }/../../configs/data.yml`, 'utf8'))
+keys(data).map(key => {
+  datasets[key] = downloadData(data[key])
+})
 
 router.route('/')
   .all((req: Request, res: Response) => {
     res.json({
-      api: 'Mobile Map Bundler v1.0.0',
-      cluster: worker.process.pid,
-      http: [
-        { method: 'GET', url: '/{zoom}/{x}/{y}(.json|.geojson|.osm)' },
+      api: 'Mobile Map Builder v1.0.0',
+      cluster: (worker) ? worker.process.pid : 0,
+      datasets: keys(data),
+      http: [,
         { method: 'GET', url: '/{zoom}/{x}/{y}/extent(.json|.geojson|.osm)' },
-        { method: 'GET',
-          source: 'https://gist.github.com/DenisCarriere/4d0ebc8126eb0c4bffe7b0bd5ee029c9',
-          url: '/{zoom}/{x}/{y}/ball-diamonds(.json|.geojson|.osm)'},
+        { method: 'GET', url: '/{zoom}/{x}/{y}/<dataset>(.json|.geojson|.osm)'},
       ],
       ok: true,
       status: 200,
     })
   })
 
-router.route('/:zoom(\\d+)/:tile_column(\\d+)/:tile_row(\\d+)(/extent(.osm|)|.osm|)')
-  .get(async (req: Request, res: Response) => {
+router.route('/:zoom(\\d+)/:tile_column(\\d+)/:tile_row(\\d+)(/extent:ext(.json|.geojson|.osm|)|:ext(.json|.geojson|.osm|))')
+  .get(async (req: InterfaceRequest, res: Response) => {
+    debug.server({ ext: req.params.ext })
+
     // Build Tile
     const tile = new Tile(req.params)
     const poly = turf.bboxPolygon(tile.bbox)
     poly.properties = tile
     poly.bbox = tile.bbox
-    const collection = turf.featureCollection([poly])
+    const extent = turf.featureCollection([poly])
 
-    // Parse OSM
-    const osm = geojson2osm(collection)
-    res.set('Content-Type', 'text/xml')
-    res.send(osm)
-  })
-
-router.route('/:zoom(\\d+)/:tile_column(\\d+)/:tile_row(\\d+)(/extent(.json|.geojson)|(.json|.geojson))')
-  .get(async (req: Request, res: Response) => {
-    // Build Tile
-    const tile = new Tile(req.params)
-    const poly = turf.bboxPolygon(tile.bbox)
-    poly.properties = tile
-    poly.bbox = tile.bbox
-    const collection = turf.featureCollection([poly])
-    res.json(collection)
-  })
-
-router.route('/:zoom(\\d+)/:tile_column(\\d+)/:tile_row(\\d+)/ball-diamonds(.json|.geojson)')
-  .get(async (req: Request, res: Response) => {
-    // Build Tile
-    const tile = new Tile(req.params)
-    const poly = turf.bboxPolygon(tile.bbox)
-    const collection = turf.featureCollection([poly])
-
-    // Only find points within
-    const within = turf.within(await ballDiamonds, collection)
-    res.json(within)
-  })
-
-router.route('/:zoom(\\d+)/:tile_column(\\d+)/:tile_row(\\d+)/ball-diamonds(.osm|)')
-  .get(async (req: Request, res: Response) => {
-    // Build Tile
-    const tile = new Tile(req.params)
-    const poly = turf.bboxPolygon(tile.bbox)
-    const collection = turf.featureCollection([poly])
-
-    // Only find points within
-    const within = turf.within(await ballDiamonds, collection)
-
+    // Output as GeoJSON or OSM
+    if (req.params.ext === '.json' || req.params.ext === '.geojson') {
+      res.json(extent)
     // Parse GeoJSON to OSM
-    const osm = geojson2osm(within)
-    res.set('Content-Type', 'text/xml')
-    res.send(osm)
+    } else {
+      const osm = geojson2osm(extent)
+      res.set('Content-Type', 'text/xml')
+      res.send(osm)
+    }
+  })
+
+router.route('/:zoom(\\d+)/:tile_column(\\d+)/:tile_row(\\d+)/:dataset([\da-zA-Z_\-]+):ext(.json|.geojson|.osm|)')
+  .get(async (req: InterfaceRequest, res: Response) => {
+    debug.server({ dataset: req.params.dataset, ext: req.params.ext })
+
+    // Build Tile
+    const tile = new Tile(req.params)
+    const extent = turf.bboxPolygon(tile.bbox)
+    const dataset: GeoJSON.FeatureCollection<any> = await datasets[req.params.dataset]
+
+    // Filter by Within or intersect
+    let within: GeoJSON.FeatureCollection<any>
+    if (dataset.features[0].geometry.type === 'Point') {
+      debug.log('within')
+      within = turf.within(dataset, turf.featureCollection([extent]))
+    } else {
+      debug.log('intersect')
+      const intersect = dataset.features.filter(feature => turf.intersect(feature, extent) )
+      within = turf.featureCollection(intersect)
+    }
+    debug.log('end')
+    debug.log(within)
+
+    // Output as GeoJSON or OSM
+    if (req.params.ext === '.json' || req.params.ext === '.geojson') {
+      res.json(within)
+    // Parse GeoJSON to OSM
+    } else {
+      const osm = geojson2osm(within)
+      res.set('Content-Type', 'text/xml')
+      res.send(osm)
+    }
   })
 
 export default router
